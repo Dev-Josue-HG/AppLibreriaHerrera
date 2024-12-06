96edbde647b5bb7308396a1f496b1f73e2f5cbdc
AxelHerrera11 <aherreras@miumg.edu.gt>
2024-12-06 15:33:01 -0600

    Hola

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,16 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.8.0"
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
