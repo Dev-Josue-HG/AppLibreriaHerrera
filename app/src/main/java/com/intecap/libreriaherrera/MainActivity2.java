27e98d7aca9ba39d05f2e35ddf3b4e41fe7d83da
AxelHerrera11 <aherreras@miumg.edu.gt>
2024-12-07 17:08:59 -0600

    Hola

--- app/src/main/java/com/intecap/libreriaherrera/MainActivity2.java
+++ app/src/main/java/com/intecap/libreriaherrera/MainActivity2.java
@@ -1,21 +1,25 @@
 package com.intecap.libreriaherrera;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity2 extends AppCompatActivity {
-
-
-
-
-    private PreferencesHelper preferencesHelper;
 
 
     @Override
@@ -23,6 +27,29 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        BottomNavigationView navView = findViewById(R.id.bnvMenuNavegacionInferior);
+
+        AppBarConfiguration abcBarraConfiguracion = new AppBarConfiguration.Builder(R.id.pagina_pefil).build();
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fcvContenedorFragmentos);
+
+        NavController nvcControladorNavegacion = navHostFragment.getNavController();
+
+        nvcControladorNavegacion.setGraph(R.navigation.nav);
+
+        NavigationUI.setupWithNavController(navView, nvcControladorNavegacion);
+
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.itmPerfil){
+                    nvcControladorNavegacion.navigate(R.id.pagina_pefil);
+                }
+                return true;
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
